dab06982b10d645e3777ba7c7f0f063ed48f2657
NishantChauhan <nishantkr0210@gmail.com>
2020-12-18 00:03:28 +0530

    Used session mgmt and Cookies to coonect servlets

--- src/com/nishant/practice/AddServlet.java
+++ src/com/nishant/practice/AddServlet.java
@@ -5,29 +5,45 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class AddServlet extends HttpServlet { //a simple class must extend HttpServlet to act as servlet
-	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException { //method name always "service"
-		//doGet and doPost can also be used to restrict values to show or not show in url
-		//but above both will be called via service method only!!
-		
+public class AddServlet extends HttpServlet { // a simple class must extend HttpServlet to act as servlet
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException { // method
+																												// name
+																												// always
+																												// "service"
+		// doGet and doPost can also be used to restrict values to show or not show in
+		// url
+		// but above both will be called via service method only!!
+
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
+
+//		req.setAttribute("k", k); //we are sending data to square servlet by putting it in request object
+
+//		RequestDispatcher rd = req.getRequestDispatcher("sq"); //to call other servlet
+//		rd.forward(req, res);
+
+//		
+//		  PrintWriter out = res.getWriter(); //this is used to print result in browser.
+//		  out.println("The result is: "+k); // ``
+//		 
 		
-		req.setAttribute("k", k); //we are sending data to square servlet by putting it in request object
+//		res.sendRedirect("sq?k="+k);  //Url re-writing..we are sending data to other servlet using this.
 		
-		RequestDispatcher rd = req.getRequestDispatcher("sq"); //to call other servlet
-		rd.forward(req, res);
+//		HttpSession session = req.getSession(); // starting the session
+//		session.setAttribute("k", k); //setting the value of k to key k
 		
-		/*
-		 * PrintWriter out = res.getWriter(); //this is used to print result in browser.
-		 * out.println("The result is: "+k); // ``
-		 */	
-		}
+		Cookie cookie = new Cookie("k",k + ""); // to convert k to string, we appended ""
+		res.addCookie(cookie);
+		
+		res.sendRedirect("sq");
+	}
 
 }
--- src/com/nishant/practice/SqServlet.java
+++ src/com/nishant/practice/SqServlet.java
@@ -3,15 +3,33 @@ package com.nishant.practice;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 
 	public void service(HttpServletRequest req , HttpServletResponse res) throws IOException {
 		
-		int k = (int) req.getAttribute("k"); //To get the data that we've sent in request object.
+//		int k = (int) req.getAttribute("k"); //To get the data that we've sent in request object.
+//		int k = Integer.parseInt(req.getParameter("k")); // to get parameter by key-value
+		
+//		HttpSession session = req.getSession(); // starting the session
+//		
+////		session.removeAttribute("k"); --to remove data from the session, so in next line,we wont get any data
+//		
+//		int k = (int)session.getAttribute("k"); // getting value that we assigned in AddServlet in session.
+
+		Cookie cookies[] = req.getCookies(); // getting all trhe cookies..in an array
+		
+		int k = 0;
+		for(Cookie c : cookies) {            // iterating through the cookies
+			if(c.getName().equals("k")) {    // finding the cookie containing value of k
+				k  = Integer.parseInt(c.getValue());  
+			}
+		}
 		
 		int s =k;
 		k= k*k;
